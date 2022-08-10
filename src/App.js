fe68769d07aaeb1921d787755850e63219382ad4
Eyobedk <Eyobedkebede10@gmail.com>
2022-08-10 23:50:14 +0100

    remove content from table and add new

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React,{ Component} from "react"
 import Table from "./Table";
-
+import Form from "./Form";
 class App extends Component{
      state = {
             characters: [
@@ -22,27 +22,27 @@ class App extends Component{
                   }
             ]
         };
-    
-        removeCharacter = index => {
-            const { characters } = this.state;
         
-            this.setState({
-                characters: characters.filter((character, i) => { 
-                    return i !== index;
-                })
-            });
+        removeCharacter = (index)=>{
+            const {characters} = this.state;
+            this.setState({characters: characters.filter((char, i)=>{
+                return i !== index;
+            })
+         });
+        };
+
+        handleSubmit = (character) =>{
+            this.setState({characters : [...this.state.characters, character]})
         }
-        
+
         render(){
        
         const { characters } = this.state;
 
         return (
             <div className="container">
-                <Table
-                    characterData={characters}
-                    removeCharacter={this.removeCharacter}
-                />
+               <Table charactersData={characters} removeCharacter={this.removeCharacter} />
+               <Form handleSubmit={this.handleSubmit}/>
             </div>
         )
     }
